993beac3fd47f77e5f4c14537ee229e2dbfc1af3
RyanHeadlee <headleeryan@gmail.com>
2024-05-07 11:53:55 -0700

    Add get_best_offers_official and get_final_redirect_url functions

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from collections import OrderedDict
+from urllib.parse import urljoin
+
+
+# Function converts redirect url to the full link after redirection
+# Params: base_url - base url to prepend (https://gg.deals),
+# relative_url - relative url to append to base_url
+# Returns: final_url - the url of the page that was redirected to
+def get_final_redirect_url(base_url, relative_url):
+    full_url = urljoin(base_url, relative_url)
+    response = requests.head(full_url, allow_redirects=True)
+    final_url = response.url
+    return final_url
 
 
 # This function finds all the game and bundle links in a GGDeals page
@@ -30,9 +42,42 @@ def init_search(search_for):
     return unique_links[:5]
 
 
+# This function finds all the important information about the searched game
+# Parameters: search_for: link chosen to enter
+# Returns: lists of the shop names, game names, prices, and links to the storefront
 def get_best_offers_official(search_for):
     response = requests.get("https://gg.deals" + search_for)
     soup = BeautifulSoup(response.text, features="html.parser")
+    official_stores_div = soup.find("div", id="official-stores")
+
+    shop_names, game_names, current_prices, links = [], [], [], []
+
+    if official_stores_div:
+        game_details = official_stores_div.find_all(
+            "div",
+            class_=[
+                "relative hoverable-box d-flex flex-wrap flex-align-center game-item cta-full item game-deals-item game-list-item keep-unmarked-container with-gift-card",
+                "relative hoverable-box d-flex flex-wrap flex-align-center game-item cta-full item game-deals-item game-list-item keep-unmarked-container",
+            ],
+        )
+        for game_detail in game_details:
+            if isinstance(game_detail, Tag):
+                # Extract the attributes
+                shop_name = game_detail.attrs.get("data-shop-name")
+                game_name = game_detail.attrs.get("data-game-name")
+                current_price = game_detail.find(
+                    "span", class_="price-inner game-price-current"
+                ).text
+                link = game_detail.find("a", class_="full-link")["href"]
+                link = get_final_redirect_url("https://gg.deals", link)
+
+                # Append the attributes
+                game_names.append(game_name)
+                shop_names.append(shop_name)
+                current_prices.append(current_price)
+                links.append(link)
+
+    return shop_names, game_names, current_prices, links
 
 
 def main():
@@ -45,7 +90,16 @@ def main():
     search_for = int(input())
     search_for = links_to_display[search_for - 1]
 
-    get_best_offers_official(search_for)
+    shop_names, game_names, current_prices, links = get_best_offers_official(search_for)
+    print("\nThe best offers from official storefronts:")
+    for i, (shop_name, game_name, price, link) in enumerate(
+        zip(shop_names, game_names, current_prices, links)
+    ):
+        print(
+            "{}. Storefront: {} Title: {} Price: {} Link: {}".format(
+                i + 1, shop_name, game_name, price, link
+            )
+        )
 
 
 main()
